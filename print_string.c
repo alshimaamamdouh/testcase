70b75d803f37fa55d1c58ea4d8572888f82a543b
nehadadel <nehadadel@gmail.com>
2023-09-17 16:56:37 -0700

    update

--- print_string.c
+++ print_string.c
@@ -8,15 +8,18 @@
 */
 int print_string(char *str)
 {
-int str_len;
+int str_len = 0;
 char *s = "(null)";
 
 if (str == NULL)
 {
-str_len = strlen(s);
+while (*(s + str_len) != '\0')
+str_len++;
 write(1, s, str_len);
 return (str_len);
 }
+while (*(str + str_len) != '\0')
+str_len++;
 str_len = strlen(str);
 write(1, str, str_len);
 return (str_len);
