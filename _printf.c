823cefe0e4558a2bfc5994657f512e212e19587f
nehadadel <nehadadel@gmail.com>
2023-09-17 13:48:57 -0700

    update

--- _printf.c
+++ _printf.c
@@ -9,34 +9,33 @@
 */
 int _printf(const char *format, ...)
 {
-int n, j, num_of_ch;
+int n, num_of_ch;
 char *s;
 char ch;
-va_list args;// Create a va_list to hold the variable arguments
+va_list args;
 
-va_start(args, format); // Initialize the va_list
+va_start(args, format);
 num_of_ch = 0;
 n = 0;
-//Iterate through the format string character by character:
+
 while (format && format[n])
 {
-//If the current character is not '%', print it directly to the output.
 if (format[n] != '%')
 {
 write(1, &format[n], 1);
 num_of_ch++;
 }
-else
+else if (format[n+1])
 {
-//Move the pointer to the next character.
+
 n = n + 1;
-//ask if the next ch is = %,so it will print it
+
 if (format[n] == '%')
 {
 write(1, &format[n], 1);
 num_of_ch++;
 }
-//ask for format specifier
+
 else if (format[n] == 'c')
 {
 ch = va_arg(args, int);
@@ -49,7 +48,7 @@ num_of_ch = num_of_ch + print_string(s);
 }
 else if (format[n] == 'i' || format[n] == 'd')
 {
-  //integer format
+  
 }
 else
 {
