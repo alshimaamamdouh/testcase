544c9ac07b1f8523a84c6dda43cde59da5842b61
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-09-18 12:35:02 -0700

    update

--- _itoa_generic.c
+++ _itoa_generic.c
@@ -8,25 +8,25 @@
  * Return: character inverted
  */
 
-char itoa_generic (char* u8ptrbuf, int u32int_value)
+char itoa_generic(char *u8ptrbuf, int u32int_value)
 {
-  char index = 0,num_digits=0;
-  int tempVariable = u32int_value;
-  if (u32int_value == 0)
-  {
-    num_digits = 1;
-  }
-  else
-  {
-    for(num_digits = 0; tempVariable > 0; num_digits++)
-    {
-      tempVariable /= 10;
-    }
-  }
-  for(index = 1; index <= num_digits; index++)
-  {
-    u8ptrbuf[num_digits - index] = (u32int_value % 10) + 0x30;
-    u32int_value /= 10;
-  }
-  return num_digits;
+char index = 0, num_digits = 0;
+int tempVariable = u32int_value;
+if (u32int_value == 0)
+{
+num_digits = 1;
+}
+else
+{
+for (num_digits = 0; tempVariable > 0; num_digits++)
+{
+tempVariable /= 10;
+}
+}
+for (index = 1; index <= num_digits; index++)
+{
+u8ptrbuf[num_digits - index] = (u32int_value % 10) + 0x30;
+u32int_value /= 10;
+}
+return (num_digits);
 }
--- _putprin.c
+++ _putprin.c
@@ -4,40 +4,35 @@
  *putprin - write function
  *@t: input one
  *@dig: input two
- *@fla:g: input three
+ *@flag: input three
  *@ss: input four
  *
  */
 
-void putprin(char *t ,   long double dig , char flag , char *ss )
+void putprin(char *t, long double dig, char flag, char *ss)
 {
-   int temp ,first;	
-   char num,s[10];
-   int x = (int) dig;
-    switch(flag)
-    {
-   case 'i':
-   num=itoa_generic(s,dig);
+int temp, first;
 
-    write(1, s, num);
-
-                     break;
-    case 'c':
-            write(1,&x, 1);
-            break;
-
-    case 's':
-                     for(first = 0 ; ss[first] != '\0'; first++)
-                     {
-                             write(1, &ss[first], 1);
-                     }
-
-
-            break;
-
+char num, s[10];
+int x = (int) dig;
+switch (flag)
+{
+case 'i':
+num = itoa_generic(s, dig);
+write(1, s, num);
+break;
+case 'c':
+write(1, &x, 1);
+break;
+case 's':
+for (first = 0 ; ss[first] != '\0'; first++)
+{
+write(1, &ss[first], 1);
 }
-                     for(temp = 2 ; t[temp] != '\0'; temp++)
-                     {
-                             write(1, &t[temp], 1);
-                     }
+break;
+}
+for (temp = 2 ; t[temp] != '\0'; temp++)
+{
+write(1, &t[temp], 1);
+}
 }
