6e0acb4662c21d37ee26466310d34f45c3cfee94
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-09-18 13:50:52 -0700

    test

--- main.c
+++ main.c
@@ -11,13 +11,18 @@ int main(void)
 {
     int len;
     int len2;
+    unsigned int ui;
 
+    ui = (unsigned int)INT_MAX + 1024;
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
-    _printf("css%ccs%scscscs", 'T',"Test");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+     _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
     return (0);
 }
